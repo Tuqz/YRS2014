e3970b8152eb7ae97f078b677ab1fc67aaa8ecfd
Tuqz <jamesrawson628@gmail.com>
2014-07-28 13:47:48 +0100

    Return time and url from url generator

--- metoffice_rain.py
+++ metoffice_rain.py
@@ -1,7 +1,7 @@
 import constants, urllib.request
 import xml.etree.ElementTree as XMLParser
 
-def url_gen(delta_time): #return a URL if successful, or False if not
+def url_gen(delta_time): #return a URL and time if successful, or False if not
 	if(delta_time < 0 or delta_time > 36):
 		return False
 	if(delta_time % 3):
@@ -22,5 +22,7 @@ def url_gen(delta_time): #return a URL if successful, or False if not
 	image_format = root[1][0][1].text
 	default_time = root[1][0][2].attrib['defaultTime']
 	URL = baseURL.format(LayerName=layer_name, ImageFormat=image_format, DefaultTime=default_time, Timestep=delta_time, key=constants.key)
-	return URL
+	return (URL, default_time)
 
+
+print(url_gen(0))
